52b339ac9abe09c562c99653bd8cc6b31292019b
Adrienne L. Travis <adrienne@utilitarienne.com>
2020-10-07 14:39:11 -0700

    updating shortcodes file

--- utilitarienne_site_utilities/libraries/Shortcodes.php
+++ utilitarienne_site_utilities/libraries/Shortcodes.php
@@ -1,6 +1,57 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Shortcodes {
+class Shortcodes
+{
 
-    
+    private $shortcodes = [];
+
+    public function __construct()
+    {
+        ee()->load->helper(['parameters']);
+
+        $this->_makeShortcode(
+            'SEASON', 
+            function ($match) {
+                $month = date('n');
+                if(in_array($month, [12, 1, 2])) {
+                    return 'Winter';
+                } elseif(in_array($month, [3, 4, 5])) {
+                    return 'Spring';
+                } elseif(in_array($month, [6, 7, 8])) {
+                    return 'Summer';
+                } else {
+                    return 'Autumn';
+                }
+            }, 
+            false
+        );
+        
+        $this->_makeShortcode(
+            'DATE', 
+            function ($match) {
+                if(isset($match[1])) {
+                    $parameters = parameterize($match[1]);
+                    $format = str_replace('%', '', $parameters['format']);
+                } else {
+                    $format = 'M-d-Y H:i';
+                }
+                return date($format);
+            }, 
+            true
+        );
+    }
+
+    public function parser($text)
+    {
+        $matches = [];
+        $count = 0;
+        return preg_replace_callback_array($this->shortcodes, $text, -1, $count);
+    }
+
+    private function _makeShortcode($shortcode, $function, $hasParams = false)
+    {
+        $code = $hasParams ? sprintf('/\[\[(?:%s)(?:\s?([^\]]+?))?\]\]/imsU', $shortcode) : sprintf('/\[\[(?:%s)\]\]/ims', $shortcode);
+        $this->shortcodes[$code] = $function;
+    }
+
 }
